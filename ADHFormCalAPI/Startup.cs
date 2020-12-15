6e4ed4b1406e18d2cb86133aa8f0b84494170771
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-15 20:05:44 +0400

    swagger setup
    create swagger page for testing the API endpoints

--- ADHFormCalAPI/Startup.cs
+++ ADHFormCalAPI/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace ADHFormCalAPI
 {
@@ -22,13 +16,56 @@ namespace ADHFormCalAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(setup =>
+            {
+
+                setup.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "ADHFormCal API",
+                        Version = "v1"
+                    });
+
+                // in case you need Auth in swagger
+
+                //setup.AddSecurityDefinition("Bearer",
+                //    new OpenApiSecurityScheme
+                //    {
+                //        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                //      Enter 'Bearer' [space] and then your token in the text input below.
+                //      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                //        Name = "Authorization",
+                //        In = ParameterLocation.Header,
+                //        Type = SecuritySchemeType.ApiKey,
+                //        Scheme = "Bearer"
+                //    });
+
+                //setup.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                //{
+                //    {
+                //        new OpenApiSecurityScheme
+                //        {
+                //            Reference = new OpenApiReference
+                //                {
+                //                Type = ReferenceType.SecurityScheme,
+                //                Id = "Bearer"
+                //                },
+                //                Scheme = "oauth2",
+                //                Name = "Bearer",
+                //                In = ParameterLocation.Header,
+
+                //        },
+                //        new List<string>()
+                //     }
+                //});
+
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,6 +78,12 @@ namespace ADHFormCalAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "ADH Api v1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
