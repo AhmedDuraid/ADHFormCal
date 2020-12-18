9a0c0cb0d67f46d6b9ad22bb16feced59df7dc83
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-18 21:01:15 +0400

    create valume model calss and it props

---
+++ ADHFormCalAPI/Models/Converters/VolumeModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ADHFormCalAPI.Models.Converters
+{
+    public class VolumeModel
+    {
+        //Metric
+        public double Liters { get; set; }
+        public double Centiliters { get; set; }
+        public double Milliliters { get; set; }
+        public double CubicMeters { get; set; }
+
+        // US
+        public double USGallons { get; set; }
+        public double USTablespoons { get; set; }
+        public double USTeaspoons { get; set; }
+
+        // UK
+        public double UKGallons { get; set; }
+        public double UKTablespoons { get; set; }
+        public double UKTeaspoons { get; set; }
+
+    }
+}
