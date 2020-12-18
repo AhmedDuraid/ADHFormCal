4dea26a52f00a9c5f784ed6eb4463661c2b927ab
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-18 13:19:58 +0400

    change to get only
    changing different temp to be read-only
    
    change some prop value type
    
    code cleaning

--- ADHFormCalAPI/Models/Converters/TemperatureModel.cs
+++ ADHFormCalAPI/Models/Converters/TemperatureModel.cs
@@ -6,28 +6,30 @@
         private readonly int _FreezingPointF = 32;
         private readonly double FahrenheitCelsiusConst = 1.8;
 
+        private double _TemperatureC, _TemperatureF, _TemperatureK;
+
         public TemperatureModel(double temperature, char symbol)
         {
             SetTemperature(temperature, symbol);
         }
 
         // Celsius 
-        public double TemperatureC { get; set; }
-        public double FreezingPointC { get; } = 0;
-        public int BoilingPointC { get; } = 100;
-        public int BodyTempC { get; } = 37;
-        public string SymbolC { get; } = "C";
+        public double TemperatureC { get => _TemperatureC; }
+        public double FreezingPointC { get => 0; }
+        public int BoilingPointC { get => 100; }
+        public int BodyTempC { get => 37; }
+        public char SymbolC { get => 'C'; }
 
         // Fahrenheit 
-        public double TemperatureF { get; set; }
-        public double FreezingPointF { get => _FreezingPointF; }
-        public int BoilingPointF { get; } = 212;
-        public string SymbolF { get; } = "F";
-        public double BodyTempF { get; } = 98.6;
+        public double TemperatureF { get => _TemperatureF; }
+        public int FreezingPointF { get => _FreezingPointF; }
+        public int BoilingPointF { get => 212; }
+        public char SymbolF { get => 'F'; }
+        public double BodyTempF { get => 98.6; }
 
         // Kelvin 
-        public double TemperatureK { get; set; }
-        public string SymbolK { get; } = "K";
+        public double TemperatureK { get => _TemperatureK; }
+        public char SymbolK { get => 'K'; }
 
         private void SetTemperature(double temperature, char symbol)
         {
@@ -36,27 +38,27 @@
                 case 'C':
                     {
 
-                        TemperatureC = temperature;
-                        TemperatureF = (temperature * FahrenheitCelsiusConst) + _FreezingPointF;
-                        TemperatureK = temperature + KelvinConst;
+                        _TemperatureC = temperature;
+                        _TemperatureF = (temperature * FahrenheitCelsiusConst) + _FreezingPointF;
+                        _TemperatureK = temperature + KelvinConst;
 
                         break;
                     }
 
                 case 'F':
                     {
-                        TemperatureF = temperature;
-                        TemperatureC = (temperature - _FreezingPointF) / FahrenheitCelsiusConst;
-                        TemperatureK = TemperatureC + KelvinConst;
+                        _TemperatureF = temperature;
+                        _TemperatureC = (temperature - _FreezingPointF) / FahrenheitCelsiusConst;
+                        _TemperatureK = TemperatureC + KelvinConst;
 
                         break;
 
                     }
                 case 'K':
                     {
-                        TemperatureK = temperature;
-                        TemperatureC = temperature - KelvinConst;
-                        TemperatureF = (TemperatureC * FahrenheitCelsiusConst) + _FreezingPointF;
+                        _TemperatureK = temperature;
+                        _TemperatureC = temperature - KelvinConst;
+                        _TemperatureF = (TemperatureC * FahrenheitCelsiusConst) + _FreezingPointF;
 
                         break;
                     }
