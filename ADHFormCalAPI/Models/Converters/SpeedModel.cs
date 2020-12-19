1354c3579f45c65c09dea2ee4029e7a382378cc3
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-19 20:52:50 +0400

    create speed class that is responsible for speed variables

---
+++ ADHFormCalAPI/Models/Converters/SpeedModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ADHFormCalAPI.Models.Converters
+{
+    public class SpeedModel
+    {
+        //worldwide
+        public double KilometersPerHour { get; set; }
+        public double Knots { get; set; }
+
+        //US
+        public double MilesPerHour { get; set; }
+
+    }
+}
