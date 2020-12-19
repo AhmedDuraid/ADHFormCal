b38aecb2dec198c37f1b6c651b300e651e1a0eea
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-19 20:36:38 +0400

    optimize the length class
    recreate SetValues function to SetMetricValues the will change metric system variables
    
    create SetImperialValues function to  change imperial system variables

--- ADHFormCalAPI/Models/Converters/LengthModel.cs
+++ ADHFormCalAPI/Models/Converters/LengthModel.cs
@@ -35,7 +35,7 @@
             get => _millimeters;
             private set => _millimeters = value < 0 ? 0 : value;
         }
-        public string MillimeterSymbol { get => "mm"; }
+        public string MillimeterSymbol => "mm";
 
         // Imperial 
         public double Miles
@@ -43,25 +43,25 @@
             get => _miles;
             private set => _miles = value < 0 ? 0 : value;
         }
-        public string MilesSymbol { get => "mi"; }
+        public string MilesSymbol => "mi";
         public double Yards
         {
             get => _yards;
             private set => _yards = value < 0 ? 0 : value;
         }
-        public string YardSymbol { get => "yd"; }
+        public string YardSymbol => "yd";
         public double Foot
         {
             get => _foot;
             private set => _foot = value < 0 ? 0 : value;
         }
-        public string FootSymbol { get => "ft"; }
+        public string FootSymbol => "ft";
         public double Inches
         {
             get => _inches;
             private set => _inches = value < 0 ? 0 : value;
         }
-        public string IncheSymbol { get => "in"; }
+        public string IncheSymbol => "in";
         private void SetLength(double value, string unit)
         {
             switch (unit)
@@ -69,81 +69,65 @@
                 case "km":
                     {
 
-                        SetValues(value, value / 0.001, value / 0.00001,
-                            value / 0.000001, value * 0.62137,
-                           value * 3280.8, value * 1093.6,
-                           value * 39370);
+                        SetMetricValues(value, value / 0.001, value / 0.00001, value / 0.000001);
+                        SetImperialValues(value * 0.62137, value * 3280.8, value * 1093.6, value * 39370);
                         break;
                     }
                 case "m":
                     {
-                        SetValues(value / 1000, value,
-                            value / 0.010, value / 0.001,
-                            value * 0.00053996, value * 3.2808,
-                            value * 1.0936, value * 39.37);
+                        SetMetricValues(value / 1000, value, value / 0.010, value / 0.001);
+                        SetImperialValues(value * 0.00053996, value * 3.2808, value * 1.0936, value * 39.37);
                         break;
                     }
                 case "cm":
                     {
-                        SetValues(value / 100000, value / 100,
-                            value, value / 0.1,
-                            value * 0.0000062137, value * 0.032808,
+                        SetMetricValues(value / 100000, value / 100, value, value / 0.1);
+                        SetImperialValues(value * 0.0000062137, value * 0.032808,
                             value * 0.010936, value * 0.39370);
                         break;
                     }
                 case "mm":
                     {
-                        SetValues(value / 1000000, value / 1000,
-                            value / 10, value,
-                            value * 0.00000062137, value * 0.0032808,
-                            value * 0.0010936, value * 0.039370);
+                        SetMetricValues(value / 1000000, value / 1000, value / 10, value);
+                        SetImperialValues(value * 0.00000062137, value * 0.0032808, value * 0.0010936, value * 0.039370);
                         break;
                     }
                 case "mi":
                     {
-                        SetValues(value / 0.62137, value / 0.00062137,
-                            value / 0.0000062137, value / 0.00000062137,
-                            value, value * 5280.0,
-                            value * 1760.0, value * 6336);
+                        SetMetricValues(value / 0.62137, value / 0.00062137, value / 0.0000062137, value / 0.00000062137);
+                        SetImperialValues(value, value * 5280.0, value * 1760.0, value * 6336);
                         break;
                     }
                 case "yd":
                     {
-                        SetValues(value / 1093.6, value / 1.0936,
-                            value / 0.010936, value / 0.0010936,
-                            value * 0.00056818, value * 3,
-                            value, value * 36);
-
-
+                        SetMetricValues(value / 1093.6, value / 1.0936, value / 0.010936, value / 0.0010936);
+                        SetImperialValues(value * 0.00056818, value * 3, value, value * 36);
                         break;
                     }
                 case "ft":
                     {
-                        SetValues(value / 3280.8, value / 3.2808,
-                            value / 0.032808, value / 0.0032808,
-                            value * 0.00018939, value,
-                            value * 0.33333, value * 12);
+                        SetMetricValues(value / 3280.8, value / 3.2808, value / 0.032808, value / 0.0032808);
+                        SetImperialValues(value * 0.00018939, value, value * 0.33333, value * 12);
                         break;
                     }
                 case "in":
                     {
-                        SetValues(value / 39370, value / 39.370,
-                            value / 0.39370, value / 0.039370,
-                            value * 0.000015783, value * 0.083333,
-                            value * 0.027778, value);
+                        SetMetricValues(value / 39370, value / 39.370, value / 0.39370, value / 0.039370);
+                        SetImperialValues(value * 0.000015783, value * 0.083333, value * 0.027778, value);
                         break;
                     }
             }
         }
 
-        private void SetValues(double kilometers, double metersc, double centimeters
-           , double millimeters, double miles, double foot, double yards, double inches)
+        private void SetMetricValues(double kilometers, double metersc, double centimeters, double millimeters)
         {
             Kilometers = kilometers;
             Meters = metersc;
             Centimeters = centimeters;
             Millimeters = millimeters;
-
+        }
+        private void SetImperialValues(double miles, double foot, double yards, double inches)
+        {
             Miles = miles;
             Foot = foot;
             Yards = yards;
