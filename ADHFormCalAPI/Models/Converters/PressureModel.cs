7894d7f88bc3b8c915ec52c9030766ad4c987f74
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-21 21:09:51 +0400

    Add pressure unit symbols
    make sure that pressure values don't get to less than 0

--- ADHFormCalAPI/Models/Converters/PressureModel.cs
+++ ADHFormCalAPI/Models/Converters/PressureModel.cs
@@ -6,10 +6,25 @@
         {
             PressureConverting(value, unit);
         }
-        public double Pascals { get; set; }
-        public double Bar { get; set; }
-        public double Torrs { get; set; }
-
+        private double _pascals, _bar, _torrs;
+        public double Pascals
+        {
+            get => _pascals;
+            set => _pascals = value < 0 ? 0 : value;
+        }
+        public string PascalsSymbol => "Pa";
+        public double Bar
+        {
+            get => _bar;
+            set => _bar = value < 0 ? 0 : value;
+        }
+        public string BarSymbol => "bar";
+        public double Torrs
+        {
+            get => _torrs;
+            set => _torrs = value < 0 ? 0 : value;
+        }
+        public string TorrsSymbol => "Torr";
 
         private void PressureConverting(double value, string unit)
         {
@@ -33,7 +48,6 @@
             }
 
         }
-
         private void SetValues(double pascals, double bar, double torrs)
         {
             Pascals = pascals;
