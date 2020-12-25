46a5d0be244410159561caa3e144ee92e2c82b1d
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-25 21:34:59 +0400

    move pressure calculation from PressuerModel

--- ADHFormCalAPI/Calculator/Converters/PressureConvertingOperations.cs
+++ ADHFormCalAPI/Calculator/Converters/PressureConvertingOperations.cs
@@ -11,11 +11,49 @@ namespace ADHFormCalAPI.Calculator.Converters
         {
             OperationUnit = operationUnit;
             Value = value;
-
         }
 
         public PressureModel PressureConverting()
         {
+            PressureModel model = new PressureModel();
+            double Pascal;
+            double Bar;
+            double Torr;
+            // pa
+            if (model.PascalsSymbol == OperationUnit)
+            {
+                Pascal = Value;
+                Bar = Value / 100000;
+                Torr = Value * 0.0075006;
+
+                SetValues(model, Pascal, Bar, Torr);
+
+                return model;
+            }
+
+            // bar
+            if (model.BarSymbol == OperationUnit)
+            {
+                Pascal = Value / 0.000010000;
+                Bar = Value;
+                Torr = Value * 750.06;
+
+                SetValues(model, Pascal, Bar, Torr);
+
+                return model;
+            }
+
+            //torr
+            if (model.TorrsSymbol == OperationUnit)
+            {
+                Pascal = Value / 0.0075006;
+                Bar = Value / 750.06;
+                Torr = Value;
+
+                SetValues(model, Pascal, Bar, Torr);
+
+                return model;
+            }
             return null;
         }
 
--- ADHFormCalAPI/Models/Converters/PressureModel.cs
+++ ADHFormCalAPI/Models/Converters/PressureModel.cs
@@ -2,57 +2,28 @@
 {
     public class PressureModel
     {
-        public PressureModel(double value, string unit)
-        {
-            PressureConverting(value, unit);
-        }
         private double _pascals, _bar, _torrs;
         public double Pascals
         {
             get => _pascals;
             set => _pascals = value < 0 ? 0 : value;
         }
-        public string PascalsSymbol => "Pa";
+        public string PascalsSymbol => "pa";
+
         public double Bar
         {
             get => _bar;
             set => _bar = value < 0 ? 0 : value;
         }
         public string BarSymbol => "bar";
+
         public double Torrs
         {
             get => _torrs;
             set => _torrs = value < 0 ? 0 : value;
         }
-        public string TorrsSymbol => "Torr";
+        public string TorrsSymbol => "torr";
 
-        private void PressureConverting(double value, string unit)
-        {
-            switch (unit)
-            {
-                case "pa":
-                    {
-                        SetValues(value, value / 100000, value * 0.0075006);
-                        break;
-                    }
-                case "bar":
-                    {
-                        SetValues(value / 0.000010000, value, value * 750.06);
-                        break;
-                    }
-                case "torr":
-                    {
-                        SetValues(value / 0.0075006, value / 750.06, value);
-                        break;
-                    }
-            }
 
-        }
-        private void SetValues(double pascals, double bar, double torrs)
-        {
-            Pascals = pascals;
-            Bar = bar;
-            Torrs = torrs;
-        }
     }
 }
