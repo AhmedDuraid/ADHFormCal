5490e3806b5f9880aa2fd604e989ed79b9ea5999
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-25 21:40:03 +0400

    Create SpeedConvertingOperations and basic functions

---
+++ ADHFormCalAPI/Calculator/Converters/SpeedConvertingOperations.cs
@@ -0,0 +1,30 @@
+﻿using ADHFormCalAPI.Models.Converters;
+
+namespace ADHFormCalAPI.Calculator.Converters
+{
+    public class SpeedConvertingOperations
+    {
+        private string OperationUnit { get; set; }
+        private double Value { get; set; }
+
+        public SpeedConvertingOperations(string operationUnit, double value)
+        {
+            OperationUnit = operationUnit;
+            Value = value;
+        }
+
+        public SpeedModel SpeedConverting()
+        {
+            return null;
+        }
+        private void SetWorldWideValues(SpeedModel model, double kilometersPerHour, double knots)
+        {
+            model.KilometersPerHour = kilometersPerHour;
+            model.Knots = knots;
+        }
+        private void SetUSValues(SpeedModel model, double milesPerHour)
+        {
+            model.MilesPerHour = milesPerHour;
+        }
+    }
+}
