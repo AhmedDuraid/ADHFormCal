c18a613240d5fac51ece703f6d0bf3a4503fb263
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-25 22:36:00 +0400

    move all logic from time model to TimeConvertingOperations class

--- ADHFormCalAPI/Calculator/Converters/TimeConvertingOperations.cs
+++ ADHFormCalAPI/Calculator/Converters/TimeConvertingOperations.cs
@@ -17,6 +17,103 @@ namespace ADHFormCalAPI.Calculator.Converters
         public TimeModel TimeConverting()
         {
             TimeModel model = new TimeModel();
+            double Second, Minute, Hour, Day, Week, Month;
+
+            //s
+            if (model.SecondSymbol.ToString() == OperationUnit)
+            {
+                Second = Value;
+                Minute = Value / 60;
+                Hour = Value / 3600;
+                Day = Value / 86400;
+                Week = Value / 604800;
+                Month = Value / 2628000;
+
+                SetHourRelatedValues(model, Second, Minute, Hour);
+                SetDayRelatedValues(model, Day, Week, Month);
+
+                return model;
+            }
+
+            //m
+            if (model.MinuteSymbol.ToString() == OperationUnit)
+            {
+                Second = Value * 60;
+                Minute = Value;
+                Hour = Value / 60;
+                Day = Value / 1440;
+                Week = Value / 10080;
+                Month = Value / 43800;
+
+                SetHourRelatedValues(model, Second, Minute, Hour);
+                SetDayRelatedValues(model, Day, Week, Month);
+
+                return model;
+            }
+
+            //h
+            if (model.HourSymbol.ToString() == OperationUnit)
+            {
+                Second = Value * 3600;
+                Minute = Value * 60;
+                Hour = Value;
+                Day = Value / 24;
+                Week = Value / 168;
+                Month = Value / 730;
+
+                SetHourRelatedValues(model, Second, Minute, Hour);
+                SetDayRelatedValues(model, Day, Week, Month);
+
+                return model;
+            }
+
+            //d
+            if (model.DaySymbol.ToString() == OperationUnit)
+            {
+                Second = Value * 86400;
+                Minute = Value * 1440;
+                Hour = Value * 24;
+                Day = Value;
+                Week = Value / 7;
+                Month = Value / 30.417;
+
+                SetHourRelatedValues(model, Second, Minute, Hour);
+                SetDayRelatedValues(model, Day, Week, Month);
+
+                return model;
+            }
+
+            //w
+            if (model.WeekSymbol.ToString() == OperationUnit)
+            {
+                Second = Value * 604800;
+                Minute = Value * 10080;
+                Hour = Value * 168;
+                Day = Value * 7;
+                Week = Value;
+                Month = Value / 4.345;
+
+                SetHourRelatedValues(model, Second, Minute, Hour);
+                SetDayRelatedValues(model, Day, Week, Month);
+
+                return model;
+            }
+
+            //M
+            if (model.MonthSymbol.ToString() == OperationUnit)
+            {
+                Second = Value * 2628000;
+                Minute = Value * 43800;
+                Hour = Value * 730;
+                Day = Value * 30.417;
+                Week = Value * 4.345;
+                Month = Value;
+
+                SetHourRelatedValues(model, Second, Minute, Hour);
+                SetDayRelatedValues(model, Day, Week, Month);
+
+                return model;
+            }
             return null;
         }
 
--- ADHFormCalAPI/Models/Converters/TimeModel.cs
+++ ADHFormCalAPI/Models/Converters/TimeModel.cs
@@ -3,10 +3,8 @@
     public class TimeModel
     {
         private double _second, _minute, _hour, _day, _week, _month;
-        public TimeModel(double value, char unit)
-        {
-            TimeConverting(value, unit);
-        }
+
+
 
         // time values can be less than 0 
         public double Second
@@ -14,91 +12,38 @@
             get => _second;
             set => _second = value < 0 ? 0 : value;
         }
-        public string SecondUnit => "s";
+        public char SecondSymbol => 's';
         public double Minute
         {
             get => _minute;
             set => _minute = value < 0 ? 0 : value;
         }
-        public string MinuteUnit => "m";
+        public char MinuteSymbol => 'm';
 
         public double Hour
         {
             get => _hour;
             set => _hour = value < 0 ? 0 : value;
         }
-        public string HourUnit => "h";
+        public char HourSymbol => 'h';
         public double Day
         {
             get => _day;
             set => _day = value < 0 ? 0 : value;
         }
+
+        public char DaySymbol => 'd';
         public double Week
         {
             get => _week;
             set => _week = value < 0 ? 0 : value;
         }
+        public char WeekSymbol => 'w';
         public double Month
         {
             get => _month;
             set => _month = value < 0 ? 0 : value;
         }
-
-        private void TimeConverting(double value, char unit)
-        {
-            switch (unit)
-            {
-                case 's':
-                    {
-                        SetHourRelatedValues(value, value / 60, value / 3600);
-                        SetDayRelatedValues(value / 86400, value / 604800, value / 2628000);
-                        break;
-                    }
-                case 'm':
-                    {
-
-                        SetHourRelatedValues(value * 60, value, value / 60);
-                        SetDayRelatedValues(value / 1440, value / 10080, value / 43800);
-                        break;
-                    }
-                case 'h':
-                    {
-                        SetHourRelatedValues(value * 3600, value * 60, value);
-                        SetDayRelatedValues(value / 24, value / 168, value / 730);
-                        break;
-                    }
-                case 'd':
-                    {
-                        SetHourRelatedValues(value * 86400, value * 1440, value * 24);
-                        SetDayRelatedValues(value, value / 7, value / 30.417);
-                        break;
-                    }
-                case 'w':
-                    {
-                        SetHourRelatedValues(value * 604800, value * 10080, value * 168);
-                        SetDayRelatedValues(value * 7, value, value / 4.345);
-                        break;
-                    }
-                case 'M':
-                    {
-                        SetHourRelatedValues(value * 2628000, value * 43800, value * 730);
-                        SetDayRelatedValues(value * 30.417, value * 4.345, value);
-                        break;
-                    }
-            }
-        }
-
-        private void SetHourRelatedValues(double second, double minute, double hour)
-        {
-            Second = second;
-            Minute = minute;
-            Hour = hour;
-        }
-        private void SetDayRelatedValues(double day, double week, double month)
-        {
-            Day = day;
-            Week = week;
-            Month = month;
-        }
+        public char MonthSymbol => 'm';
     }
 }
