5b4d8f0735d8330985f7683f474f7c038ea961ca
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-25 21:52:36 +0400

    Move converting functions to SpeedConvertingOperations calss

--- ADHFormCalAPI/Calculator/Converters/SpeedConvertingOperations.cs
+++ ADHFormCalAPI/Calculator/Converters/SpeedConvertingOperations.cs
@@ -15,6 +15,48 @@ namespace ADHFormCalAPI.Calculator.Converters
 
         public SpeedModel SpeedConverting()
         {
+            SpeedModel model = new SpeedModel();
+            double KilometersPerHour;
+            double Knots;
+            double MilesPerHour;
+
+            //kmph
+            if (model.KilometersPerHourSymbol == OperationUnit)
+            {
+                KilometersPerHour = Value;
+                Knots = Value / 1.852;
+                MilesPerHour = Value / 1.609;
+
+                SetWorldWideValues(model, KilometersPerHour, Knots);
+                SetUSValues(model, MilesPerHour);
+
+                return model;
+            }
+
+            //kn
+            if (model.KnotsSymbol == OperationUnit)
+            {
+                KilometersPerHour = Value * 1.852;
+                Knots = Value;
+                MilesPerHour = Value * 1.151;
+
+                SetWorldWideValues(model, KilometersPerHour, Knots);
+                SetUSValues(model, MilesPerHour);
+
+                return model;
+            }
+
+            //mph
+            if (model.MilesPerHourSymbol == OperationUnit)
+            {
+                KilometersPerHour = Value * 1.609;
+                Knots = Value / 1.151;
+                MilesPerHour = Value;
+
+                SetWorldWideValues(model, KilometersPerHour, Knots);
+                SetUSValues(model, MilesPerHour);
+            }
+
             return null;
         }
         private void SetWorldWideValues(SpeedModel model, double kilometersPerHour, double knots)
--- ADHFormCalAPI/Models/Converters/SpeedModel.cs
+++ ADHFormCalAPI/Models/Converters/SpeedModel.cs
@@ -3,10 +3,6 @@
     public class SpeedModel
     {
         private double _kilometersPerHour, _milesPerHour, _knots;
-        public SpeedModel(double values, string unit)
-        {
-            SpeedConverting(values, unit);
-        }
 
         //worldwide
         public double KilometersPerHour
@@ -14,7 +10,9 @@
             get => _kilometersPerHour;
             set => _kilometersPerHour = value < 0 ? 0 : value;
         }
-        public string KilometersPerHourSymbol => "km/h";
+        public string KilometersPerHourSymbol => "kmph";
+        public string KilometersPerHourUnit => "km/h";
+
         public double Knots
         {
             get => _knots;
@@ -29,38 +27,5 @@
             set => _milesPerHour = value < 0 ? 0 : value;
         }
         public string MilesPerHourSymbol => "mph";
-        private void SpeedConverting(double value, string unit)
-        {
-            switch (unit)
-            {
-                case "km-h":
-                    {
-                        SetWorldWideValues(value, value / 1.852);
-                        SetUSValues(value / 1.609);
-                        break;
-                    }
-                case "m-p-h":
-                    {
-                        SetWorldWideValues(value * 1.609, value / 1.151);
-                        SetUSValues(value);
-                        break;
-                    }
-                case "knots":
-                    {
-                        SetWorldWideValues(value * 1.852, value);
-                        SetUSValues(value * 1.151);
-                        break;
-                    }
-            }
-        }
-        private void SetWorldWideValues(double kilometersPerHour, double knots)
-        {
-            KilometersPerHour = kilometersPerHour;
-            Knots = knots;
-        }
-        private void SetUSValues(double milesPerHour)
-        {
-            MilesPerHour = milesPerHour;
-        }
     }
 }
