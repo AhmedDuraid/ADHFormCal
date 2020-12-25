d419af5a603d8c13b629a5431d71b96e7290ae4e
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-25 22:11:36 +0400

    move temperature logic to TemperatureConvertingOperations class

---
+++ ADHFormCalAPI/Calculator/Converters/TemperatureConvertingOperations.cs
@@ -0,0 +1,71 @@
+﻿using ADHFormCalAPI.Models.Converters;
+
+namespace ADHFormCalAPI.Calculator.Converters
+{
+    public class TemperatureConvertingOperations
+    {
+        private string OperationUnit { get; set; }
+        private double Value { get; set; }
+        private double FahrenheitCelsiusConst => 1.8;
+        private double KelvinConst => 273.15;
+
+        public TemperatureConvertingOperations(string operationUnit, double value)
+        {
+            OperationUnit = operationUnit;
+            Value = value;
+        }
+
+        public TemperatureModel TemperatureConverting()
+        {
+            TemperatureModel model = new TemperatureModel();
+            double TempC;
+            double TempF;
+            double TempK;
+
+            // C
+            if (OperationUnit == model.SymbolC.ToString())
+            {
+                TempC = Value;
+                TempF = (Value * FahrenheitCelsiusConst) + model.FreezingPointF;
+                TempK = Value + KelvinConst;
+
+                SetTempreatureValues(model, TempC, TempF, TempK);
+
+                return model;
+            }
+
+            //F
+            if (OperationUnit == model.SymbolC.ToString())
+            {
+                TempC = (Value - model.FreezingPointF) / FahrenheitCelsiusConst;
+                TempF = Value;
+                TempK = (Value - model.FreezingPointF) / KelvinConst;
+
+                SetTempreatureValues(model, TempC, TempF, TempK);
+
+                return model;
+            }
+
+            //K
+            if (OperationUnit == model.SymbolC.ToString())
+            {
+
+                TempC = Value - KelvinConst;
+                TempF = ((Value - KelvinConst) * model.FreezingPointF) + model.FreezingPointF;
+                TempK = Value;
+
+                SetTempreatureValues(model, TempC, TempF, TempK);
+
+                return model;
+            }
+            return null;
+        }
+
+        private void SetTempreatureValues(TemperatureModel model, double c, double f, double k)
+        {
+            model.TemperatureC = c;
+            model.TemperatureF = f;
+            model.TemperatureK = k;
+        }
+    }
+}
--- ADHFormCalAPI/Models/Converters/TemperatureModel.cs
+++ ADHFormCalAPI/Models/Converters/TemperatureModel.cs
@@ -2,15 +2,6 @@
 {
     public class TemperatureModel
     {
-        private readonly double _kelvinConst = 273.15;
-        private readonly int _freezingPointF = 32;
-        private readonly double _fahrenheitCelsiusConst = 1.8;
-
-        public TemperatureModel(double temperature, char unit)
-        {
-            TemperatureConverting(temperature, unit);
-        }
-
         // Celsius 
         public double TemperatureC { get; set; }
         public double FreezingPointC => 0;
@@ -20,7 +11,7 @@
 
         // Fahrenheit 
         public double TemperatureF { get; set; }
-        public int FreezingPointF => _freezingPointF;
+        public int FreezingPointF => 32;
         public int BoilingPointF => 212;
         public char SymbolF => 'F';
         public double BodyTempF => 98.6;
@@ -28,47 +19,5 @@
         // Kelvin 
         public double TemperatureK { get; set; }
         public char SymbolK => 'K';
-
-        private void TemperatureConverting(double temperature, char unit)
-        {
-            switch (unit)
-            {
-                case 'c':
-                    {
-                        SetTemperatureCValue(temperature);
-                        SetTemperatureFValue((temperature * _fahrenheitCelsiusConst) + _freezingPointF);
-                        SetTemperatureKValue(temperature + _kelvinConst);
-                        break;
-                    }
-
-                case 'f':
-                    {
-                        SetTemperatureCValue((temperature - _freezingPointF) / _fahrenheitCelsiusConst);
-                        SetTemperatureFValue(temperature);
-                        SetTemperatureKValue((temperature - _freezingPointF) / _kelvinConst);
-                        break;
-                    }
-                case 'k':
-                    {
-                        SetTemperatureCValue(temperature - _kelvinConst);
-                        SetTemperatureFValue(((temperature - _kelvinConst) * _freezingPointF) + _freezingPointF);
-                        SetTemperatureKValue(temperature);
-                        break;
-                    }
-            }
-        }
-
-        private void SetTemperatureCValue(double temperatureC)
-        {
-            TemperatureC = temperatureC;
-        }
-        private void SetTemperatureFValue(double temperatureF)
-        {
-            TemperatureF = temperatureF;
-        }
-        private void SetTemperatureKValue(double temperatureK)
-        {
-            TemperatureK = temperatureK;
-        }
     }
 }
