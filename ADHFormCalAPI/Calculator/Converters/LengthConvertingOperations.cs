15fafba393cd3e7e121ed64063ac537d152f3331
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-25 16:22:13 +0400

    remove LengthModel logic to separate class

---
+++ ADHFormCalAPI/Calculator/Converters/LengthConvertingOperations.cs
@@ -0,0 +1,114 @@
+﻿using ADHFormCalAPI.Models.Converters;
+
+namespace ADHFormCalAPI.Calculator.Converters
+{
+    public class LengthConvertingOperations
+    {
+        private string OperationUnit { get; set; }
+        private double Value { get; set; }
+
+        public LengthConvertingOperations(string operationUnit, double value)
+        {
+            OperationUnit = operationUnit;
+            Value = value;
+
+        }
+
+        public LengthModel LengthConverting()
+        {
+            LengthModel model = new LengthModel();
+
+            //km
+            if (model.KilometerSymbol == OperationUnit)
+            {
+                LengthMetricConverting(model, Value, Value / 0.001, Value / 0.00001, Value / 0.000001);
+                LengthImperialConverting(model, Value * 0.62137, Value * 3280.8, Value * 1093.6, Value * 39370);
+
+                return model;
+            }
+
+            // m
+            if (model.MeterSymbol == OperationUnit)
+            {
+                LengthMetricConverting(model, Value / 1000, Value, Value / 0.010, Value / 0.001);
+                LengthImperialConverting(model, Value * 0.00062137, Value * 3.2808, Value * 1.0936, Value * 39.37);
+
+                return model;
+            }
+
+            //cm
+            if (model.CentimeterSymbol == OperationUnit)
+            {
+                LengthMetricConverting(model, Value / 100000, Value / 100, Value, Value / 0.1);
+                LengthImperialConverting(model, Value * 0.0000062137, Value * 0.032808, Value * 0.010936, Value * 0.39370);
+
+                return model;
+            }
+
+            //mm
+            if (model.MillimeterSymbol == OperationUnit)
+            {
+                LengthMetricConverting(model, Value / 1000000, Value / 1000, Value / 10, Value);
+                LengthImperialConverting(model, Value * 0.00000062137, Value * 0.0032808, Value * 0.0010936, Value * 0.039370);
+
+                return model;
+            }
+
+            //mi
+            if (model.MilesSymbol == OperationUnit)
+            {
+                LengthMetricConverting(model, Value / 0.62137, Value / 0.00062137, Value / 0.0000062137, Value / 0.00000062137);
+                LengthImperialConverting(model, Value, Value * 5280.0, Value * 1760.0, Value * 6336);
+
+                return model;
+            }
+
+            //yi
+            if (model.YardSymbol == OperationUnit)
+            {
+                LengthMetricConverting(model, Value / 1093.6, Value / 1.0936, Value / 0.010936, Value / 0.0010936);
+                LengthImperialConverting(model, Value * 0.00056818, Value * 3, Value, Value * 36);
+
+                return model;
+            }
+
+            //ft
+            if (model.FootSymbol == OperationUnit)
+            {
+                LengthMetricConverting(model, Value / 3280.8, Value / 3.2808, Value / 0.032808, Value / 0.0032808);
+                LengthImperialConverting(model, Value * 0.00018939, Value, Value * 0.33333, Value * 12);
+
+                return model;
+            }
+
+            //in
+            if (model.IncheSymbol == OperationUnit)
+            {
+                LengthMetricConverting(model, Value / 39370, Value / 39.370, Value / 0.39370, Value / 0.039370);
+                LengthImperialConverting(model, Value * 0.000015783, Value * 0.083333, Value * 0.027778, Value);
+
+                return model;
+            }
+            return model;
+        }
+
+
+        private void LengthMetricConverting(LengthModel model, double kilometers, double metersc, double centimeters, double millimeters)
+        {
+            model.Kilometers = kilometers;
+            model.Meters = metersc;
+            model.Centimeters = centimeters;
+            model.Millimeters = millimeters;
+        }
+        private void LengthImperialConverting(LengthModel model, double miles, double foot, double yards, double inches)
+        {
+            model.Miles = miles;
+            model.Foot = foot;
+            model.Yards = yards;
+            model.Inches = inches;
+        }
+
+
+
+    }
+}
--- ADHFormCalAPI/Models/Converters/LengthModel.cs
+++ ADHFormCalAPI/Models/Converters/LengthModel.cs
@@ -5,35 +5,31 @@
         private double _kilometers, _meters, _centimeters
             , _millimeters, _miles, _yards, _foot, _inches;
 
-        public LengthModel(double value, string unit)
-        {
-            LengthConverting(value, unit);
-        }
         // length values can be less than 0
 
         // Metric 
         public double Kilometers
         {
             get => _kilometers;
-            private set => _kilometers = value < 0 ? 0 : value;
+            set => _kilometers = value < 0 ? 0 : value;
         }
         public string KilometerSymbol => "km";
         public double Meters
         {
             get => _meters;
-            private set => _meters = value < 0 ? 0 : value;
+            set => _meters = value < 0 ? 0 : value;
         }
         public string MeterSymbol => "m";
         public double Centimeters
         {
             get => _centimeters;
-            private set => _centimeters = value < 0 ? 0 : value;
+            set => _centimeters = value < 0 ? 0 : value;
         }
         public string CentimeterSymbol => "cm";
         public double Millimeters
         {
             get => _millimeters;
-            private set => _millimeters = value < 0 ? 0 : value;
+            set => _millimeters = value < 0 ? 0 : value;
         }
         public string MillimeterSymbol => "mm";
 
@@ -41,96 +37,27 @@
         public double Miles
         {
             get => _miles;
-            private set => _miles = value < 0 ? 0 : value;
+            set => _miles = value < 0 ? 0 : value;
         }
         public string MilesSymbol => "mi";
         public double Yards
         {
             get => _yards;
-            private set => _yards = value < 0 ? 0 : value;
+            set => _yards = value < 0 ? 0 : value;
         }
         public string YardSymbol => "yd";
         public double Foot
         {
             get => _foot;
-            private set => _foot = value < 0 ? 0 : value;
+            set => _foot = value < 0 ? 0 : value;
         }
         public string FootSymbol => "ft";
         public double Inches
         {
             get => _inches;
-            private set => _inches = value < 0 ? 0 : value;
+            set => _inches = value < 0 ? 0 : value;
         }
         public string IncheSymbol => "in";
-        private void LengthConverting(double value, string unit)
-        {
-            switch (unit)
-            {
-                case "km":
-                    {
 
-                        SetMetricValues(value, value / 0.001, value / 0.00001, value / 0.000001);
-                        SetImperialValues(value * 0.62137, value * 3280.8, value * 1093.6, value * 39370);
-                        break;
-                    }
-                case "m":
-                    {
-                        SetMetricValues(value / 1000, value, value / 0.010, value / 0.001);
-                        SetImperialValues(value * 0.00062137, value * 3.2808, value * 1.0936, value * 39.37);
-                        break;
-                    }
-                case "cm":
-                    {
-                        SetMetricValues(value / 100000, value / 100, value, value / 0.1);
-                        SetImperialValues(value * 0.0000062137, value * 0.032808,
-                            value * 0.010936, value * 0.39370);
-                        break;
-                    }
-                case "mm":
-                    {
-                        SetMetricValues(value / 1000000, value / 1000, value / 10, value);
-                        SetImperialValues(value * 0.00000062137, value * 0.0032808, value * 0.0010936, value * 0.039370);
-                        break;
-                    }
-                case "mi":
-                    {
-                        SetMetricValues(value / 0.62137, value / 0.00062137, value / 0.0000062137, value / 0.00000062137);
-                        SetImperialValues(value, value * 5280.0, value * 1760.0, value * 6336);
-                        break;
-                    }
-                case "yd":
-                    {
-                        SetMetricValues(value / 1093.6, value / 1.0936, value / 0.010936, value / 0.0010936);
-                        SetImperialValues(value * 0.00056818, value * 3, value, value * 36);
-                        break;
-                    }
-                case "ft":
-                    {
-                        SetMetricValues(value / 3280.8, value / 3.2808, value / 0.032808, value / 0.0032808);
-                        SetImperialValues(value * 0.00018939, value, value * 0.33333, value * 12);
-                        break;
-                    }
-                case "in":
-                    {
-                        SetMetricValues(value / 39370, value / 39.370, value / 0.39370, value / 0.039370);
-                        SetImperialValues(value * 0.000015783, value * 0.083333, value * 0.027778, value);
-                        break;
-                    }
-            }
-        }
-        private void SetMetricValues(double kilometers, double metersc, double centimeters, double millimeters)
-        {
-            Kilometers = kilometers;
-            Meters = metersc;
-            Centimeters = centimeters;
-            Millimeters = millimeters;
-        }
-        private void SetImperialValues(double miles, double foot, double yards, double inches)
-        {
-            Miles = miles;
-            Foot = foot;
-            Yards = yards;
-            Inches = inches;
-        }
     }
 }
