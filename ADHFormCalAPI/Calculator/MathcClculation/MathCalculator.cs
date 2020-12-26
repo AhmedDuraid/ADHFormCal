b94020b926e78591bb99355a3707010145a7399f
AhmedDuraid <ahmeduraid123@gmail.com>
2020-12-26 18:16:17 +0400

    Add Math calculations calss as math formula model

---
+++ ADHFormCalAPI/Calculator/MathcClculation/MathCalculator.cs
@@ -0,0 +1,81 @@
+﻿using ADHFormCalAPI.Models;
+using ADHFormCalAPI.Models.Math;
+using System;
+using System.Collections.Generic;
+
+namespace ADHFormCalAPI.Calculator.MathcClculation
+{
+    public class MathCalculator
+    {
+        private readonly MathFormulaModel MathFormula = new MathFormulaModel();
+        public CalculationResultModel Mean(List<double> values)
+        {
+            int totalNumbers = values.Count;
+            double sumOfValues = 0;
+            double average;
+
+            foreach (var item in values)
+            {
+                sumOfValues += item;
+            }
+
+            average = sumOfValues / totalNumbers;
+
+            return SetResult(average, MathFormula.Mean, "Mean");
+        }
+        public CalculationResultModel Probability(int targetOutcomes, int totalOutcomes)
+        {
+            double probability = targetOutcomes / totalOutcomes;
+            probability *= 100;
+
+            return SetResult(probability, MathFormula.Probability, "Probability", "%");
+        }
+        public CalculationResultModel Distance(List<int> pointOne, List<int> pointTwo)
+        {
+            // note >>> check if the list has more than 2 values 
+
+            //[x1,y1], [x2,y2]
+            int xValues = pointTwo[0] - pointOne[0];
+            int yValues = pointTwo[1] - pointOne[1];
+            double d = Math.Sqrt(Math.Pow(xValues, 2) + Math.Pow(yValues, 2));
+
+            return SetResult(d, MathFormula.Distance, "Distance");
+        }
+        public CalculationResultModel Slope(List<int> pointOne, List<int> pointTwo)
+        {
+            // note >>> check if the list has more than 2 values 
+
+            //[x1,y1], [x2,y2]
+            int xValue = pointTwo[0] - pointOne[0];
+            int YValue = pointTwo[1] - pointOne[1];
+            int slope = YValue / xValue;
+
+            return SetResult(slope, MathFormula.Slope, "Slope");
+        }
+
+        private CalculationResultModel SetResult(double value, string formula, string WhatCalculate, string unit = null, string message = null)
+        {
+            CalculationResultModel calculationResult = new CalculationResultModel
+            {
+                Formula = formula,
+                Value = value,
+                Unit = unit,
+                WhatCalculate = WhatCalculate,
+                Message = message
+            };
+
+            return calculationResult;
+        }
+
+        private CalculationResultModel Error(string message)
+        {
+            CalculationResultModel calculationResult = new CalculationResultModel
+            {
+                Error = true,
+                Message = message
+            };
+
+            return calculationResult;
+        }
+    }
+}
---
+++ ADHFormCalAPI/Models/Math/MathFormulaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ADHFormCalAPI.Models.Math
+{
+    public class MathFormulaModel
+    {
+        public string Mean => "Average=Sum of values / Number of values";
+        public string Probability => "Probability=Target outcomes / Total outcomes";
+        public string Quadratic => " x = −b ± √b²-4ac/2a";
+        public string Distance => "d=√(x₁ – x₂)² + (y₁ – y₂)²";
+        public string Slope => "Slope = y₂ – y₁ /  x₂ – x₁";
+        public string Midpoint => "(x₁+x₂) / 2, (y₁+y₂) / 2";
+    }
+}
